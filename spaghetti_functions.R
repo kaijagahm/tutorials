fe2c9072fd2213124307c3397a3a6cc821df7edd
Kaija Gahm <kaija.gahm@gmail.com>
2020-01-31 20:34:27 -0500

    changed "ingredient" argument to "what". Added drain, fill, and add_pasta functions

--- spaghetti_functions.R
+++ spaghetti_functions.R
@@ -2,22 +2,39 @@
 # Created by Kaija Gahm
 # 31 January 2020
 
-# You are given a box of pasta
-box <- data.frame(ingredient = "pasta", cooked = 0)
+# You are given a box of pasta and an empty pot
+box <- data.frame(ingredient = as.character("pasta"), cooked = 0)
+pot <- data.frame(ingredient = as.character("air"), cooked = 0)
 
-# Heat the water
-cook <- function(df, ingredient = NULL, minutes = 1){
-  if(is.null(ingredient)){
+# Fill the pot
+fill <- function(df){
+  df %>% mutate(ingredient = recode(ingredient, "air" = "water")) %>% return()
+}
+
+# Add pasta to the pot
+add_pasta <- function(df){
+  full_join(df, box) %>% return()
+}
+
+# Cook (heat) the pasta (water)
+cook <- function(df, what = NULL, minutes = 1){
+  if(is.null(what)){
     stop("Which ingredient would you like to cook?")
-  } else if(ingredient == "pasta" & !("water" %in% df$ingredient)){
+  } else if(what == "pasta" & !("water" %in% df$ingredient)){
     stop("If you try to cook pasta without water, it'll burn! Don't do that.")
-  } else if(ingredient %in% c("pasta", "water") & !(ingredient %in% df$ingredient)){
+  } else if(what %in% c("pasta", "water") & !(what %in% df$ingredient)){
     stop("That ingredient isn't in the pot yet.")
-  } else if(!(ingredient %in% c("pasta", "water"))){
+  } else if(!(what %in% c("pasta", "water"))){
     stop("Ew, why are you putting THAT in your pasta water? Try again.")
   } else{
-  df <- df %>% mutate(cooked = case_when(cooked + minutes < 10 ~ cooked + minutes,
-                                     cooked + minutes >= 10 ~ 10))
-  return(df)
+    df <- df %>% mutate(cooked = case_when(cooked + minutes < 10 & ingredient == what ~ cooked + minutes,
+                                           cooked + minutes >= 10 & ingredient == what ~ 10,
+                                           TRUE ~ cooked)) %>% return()
   } 
+}
+
+
+# Strain the pasta from the water
+drain <- function(df){
+  df %>% filter(ingredient != "water") %>% return()
 }
